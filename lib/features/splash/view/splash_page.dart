d9191658845ae60407bb7cb54d1dbd3bb4ae64e0
Dheeraj Prajapat <dheerajprajapati334@gmail.com>
2025-02-11 23:14:10 +0530

    feat : Design Splash screen and handle user authentication (if user is already loggedIn or not)

---
+++ lib/features/splash/view/splash_page.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:housekeeping_pro/configuration/assets/app_images.dart';
+import 'package:housekeeping_pro/features/%20dashboard/view/dashboard_page.dart';
+import 'package:housekeeping_pro/features/authenticaiton/view/login_page.dart';
+import 'package:housekeeping_pro/storage/local_storage.dart';
+import 'package:housekeeping_pro/storage/local_storage_constants.dart';
+
+class SplashPage extends StatefulWidget {
+  const SplashPage({super.key});
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    WidgetsBinding.instance.addPostFrameCallback((_) async {
+      final loginStatus =
+          LocalStorage().getBool(key: LocalStorageConstants.isUserLoggedIn);
+      if (!loginStatus) {
+        Future.delayed(const Duration(seconds: 2), () async {
+          if (mounted) {
+            Navigator.pushReplacementNamed(context, LoginPage.route);
+          }
+        });
+        return;
+      }
+      Future.delayed(const Duration(seconds: 2), () async {
+        if (mounted) {
+          Navigator.pushReplacementNamed(context, DashboardPage.route);
+        }
+      });
+      return;
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Container(
+        alignment: Alignment.center,
+        margin: EdgeInsets.symmetric(horizontal: 20),
+        child: Image.asset(
+          AppImages.logo,
+          color: Colors.white,
+          colorBlendMode: BlendMode.color,
+        ),
+      ),
+    );
+  }
+}
