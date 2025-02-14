6899c3b534da1c95a919fff0b9f50aa4446b07cd
Dheeraj Prajapat <dheerajprajapati334@gmail.com>
2025-02-14 12:01:10 +0530

    chore: add new routes(Dashboard, UserProfile and ChangePassword)

--- lib/configuration/route/naviagtion_routes.dart
+++ lib/configuration/route/naviagtion_routes.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:housekeeping_pro/features/%20dashboard/view/dashboard_page.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:housekeeping_pro/features/authenticaiton/view/login_page.dart';
+import 'package:housekeeping_pro/features/change_password/view/change_password_page.dart';
+import 'package:housekeeping_pro/features/change_password/view_model/change_password_bloc.dart';
+import 'package:housekeeping_pro/features/dashboard/view/dashboard_page.dart';
+import 'package:housekeeping_pro/features/dashboard/view_model/dashboard_bloc.dart';
+import 'package:housekeeping_pro/features/user_profile/view/user_profile_page.dart';
 
 class NavigationRoutes {
   factory NavigationRoutes() => _instance;
@@ -17,9 +22,23 @@ class NavigationRoutes {
         return MaterialPageRoute(
           builder: (_) => LoginPage(),
         );
+      case ChangePasswordPage.route:
+        return MaterialPageRoute(
+          builder: (_) => BlocProvider(
+            create: (context) => ChangePasswordBloc(),
+            child: ChangePasswordPage(),
+          ),
+        );
+      case UserProfilePage.route:
+        return MaterialPageRoute(
+          builder: (_) => UserProfilePage(),
+        );
       case DashboardPage.route:
         return MaterialPageRoute(
-          builder: (_) => DashboardPage(),
+          builder: (_) => BlocProvider(
+            create: (context) => DashboardBloc(),
+            child: DashboardPage(),
+          ),
         );
       default:
         return MaterialPageRoute(
