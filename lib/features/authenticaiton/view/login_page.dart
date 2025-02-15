9c7e32eef698bb8e85e551382c8aab638cc819ba
Dheeraj Prajapat <dheerajprajapati334@gmail.com>
2025-02-15 18:00:49 +0530

    feat: Design the ‘Remember Me’ UI and implement functionality to fetch/set the data if the user has checked the ‘Remember Me’ status while logging out

--- lib/features/authenticaiton/view/login_page.dart
+++ lib/features/authenticaiton/view/login_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:housekeeping_pro/common/constants/app_messages.dart';
 import 'package:housekeeping_pro/common/constants/app_strings.dart';
 import 'package:housekeeping_pro/common/toast_message/toast_message.dart';
 import 'package:housekeeping_pro/common/validators/form_validators.dart';
@@ -10,6 +11,8 @@ import 'package:housekeeping_pro/common/widgets/text_widget.dart';
 import 'package:housekeeping_pro/configuration/size_config/size_config.dart';
 import 'package:housekeeping_pro/features/authenticaiton/view_model/authentication_bloc.dart';
 import 'package:housekeeping_pro/features/dashboard/view/dashboard_page.dart';
+import 'package:housekeeping_pro/storage/local_storage.dart';
+import 'package:housekeeping_pro/storage/local_storage_constants.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -25,6 +28,29 @@ class _LoginPageState extends State<LoginPage> {
   final passwordController = TextEditingController();
   final hotelNameController = TextEditingController();
   final _formKey = GlobalKey<FormState>();
+
+  @override
+  void initState() {
+    Future.microtask(
+      () async {
+        final isRememberMe = await LocalStorage.getInstance()
+            .getBool(key: LocalStorageConstants.isRememberMe);
+        if (isRememberMe) {
+          final email = await LocalStorage.getInstance()
+              .getString(key: LocalStorageConstants.userEmail);
+          final hotelName = await LocalStorage.getInstance()
+              .getString(key: LocalStorageConstants.hotelName);
+          emailController.text = email;
+          hotelNameController.text = hotelName;
+          if (mounted) {
+            context.read<AuthenticationBloc>().add(UpdateRememberMeStatus());
+          }
+          setState(() {});
+        }
+      },
+    );
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -50,95 +76,118 @@ class _LoginPageState extends State<LoginPage> {
           return StateLoaderView(
             isLoading: state.loginStatus == AuthStatus.loading ||
                 state.encryptStatus == AuthStatus.loading,
-            child: Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Padding(
-                    padding: EdgeInsets.only(left: 20.w),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Center(
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 50.w),
                     child: TextWidget(
-                      text: AppStrings.login,
-                      color: Colors.white,
-                      fontSize: 36.sp,
-                      fontWeight: FontWeight.bold,
+                      text: AppMessages.loginScreenMessage,
+                      textAlign: TextAlign.center,
+                      fontSize: 18.sp,
                     ),
                   ),
-                  Container(
-                    margin:
-                        EdgeInsets.symmetric(horizontal: 20.w, vertical: 10.h),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20.r),
-                      color: Colors.white,
-                    ),
-                    padding:
-                        EdgeInsets.symmetric(horizontal: 20.w, vertical: 30.h),
-                    child: SingleChildScrollView(
-                      child: Form(
-                        key: _formKey,
-                        child: Column(
-                          mainAxisSize: MainAxisSize.min,
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            TextWidget(
-                              text: AppStrings.email,
-                              fontSize: 16.sp,
+                ),
+                SingleChildScrollView(
+                  child: Container(
+                    padding: EdgeInsets.symmetric(horizontal: 25.w),
+                    margin: EdgeInsets.only(top: 40.h),
+                    child: Form(
+                      key: _formKey,
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          TextWidget(
+                            text: AppStrings.email,
+                            fontSize: 16.sp,
+                          ),
+                          SizedBox(
+                            height: 1.h,
+                          ),
+                          TextInputField(
+                            controller: emailController,
+                            validator: FormValidators().validateEmail,
+                            hintText: AppStrings.email,
+                            suffixIcon: Icons.email,
+                          ),
+                          TextWidget(
+                            text: AppStrings.password,
+                            fontSize: 16.sp,
+                          ),
+                          SizedBox(
+                            height: 1.h,
+                          ),
+                          TextInputField(
+                            controller: passwordController,
+                            obscureText: true,
+                            validator: FormValidators().validatePassword,
+                            hintText: AppStrings.password,
+                          ),
+                          TextWidget(
+                            text: AppStrings.hotelName,
+                            fontSize: 16.sp,
+                          ),
+                          SizedBox(
+                            height: 1.h,
+                          ),
+                          TextInputField(
+                            controller: hotelNameController,
+                            hintText: AppStrings.hotelName,
+                            validator: FormValidators().validateHotelName,
+                          ),
+                          InkWell(
+                            onTap: () => context
+                                .read<AuthenticationBloc>()
+                                .add(UpdateRememberMeStatus()),
+                            child: Row(
+                              spacing: 10.w,
+                              children: [
+                                SizedBox(
+                                  height: 20.h,
+                                  width: 20.w,
+                                  child: Checkbox(
+                                    value: state.isRememberMe,
+                                    onChanged: (v) {
+                                      context
+                                          .read<AuthenticationBloc>()
+                                          .add(UpdateRememberMeStatus());
+                                    },
+                                  ),
+                                ),
+                                TextWidget(
+                                  text: AppStrings.rememberMe,
+                                )
+                              ],
                             ),
-                            SizedBox(
-                              height: 5.h,
-                            ),
-                            TextInputField(
-                              controller: emailController,
-                              validator: FormValidators().validateEmail,
-                              hintText: AppStrings.email,
-                            ),
-                            TextWidget(
-                              text: AppStrings.password,
-                              fontSize: 16.sp,
-                            ),
-                            SizedBox(
-                              height: 5.h,
-                            ),
-                            TextInputField(
-                              controller: passwordController,
-                              obscureText: true,
-                              validator: FormValidators().validatePassword,
-                              hintText: AppStrings.password,
-                            ),
-                            TextWidget(
-                              text: AppStrings.hotelName,
-                              fontSize: 16.sp,
-                            ),
-                            SizedBox(
-                              height: 5.h,
-                            ),
-                            TextInputField(
-                              controller: hotelNameController,
-                              hintText: AppStrings.hotelName,
-                              validator: FormValidators().validateHotelName,
-                            ),
-                            Button(
-                              onPressed: () {
-                                if (_formKey.currentState!.validate()) {
-                                  context.read<AuthenticationBloc>().add(
-                                        LoginEvent(
-                                          email: emailController.text,
-                                          password: passwordController.text,
-                                          hotelName: hotelNameController.text,
-                                        ),
-                                      );
-                                }
-                              },
-                              buttonLabel: AppStrings.login,
-                            ),
-                          ],
-                        ),
+                          ),
+                          SizedBox(
+                            height: 30.h,
+                          ),
+                          Button(
+                            onPressed: () {
+                              if (_formKey.currentState!.validate()) {
+                                context.read<AuthenticationBloc>().add(
+                                      LoginEvent(
+                                        email: emailController.text,
+                                        password: passwordController.text,
+                                        hotelName: hotelNameController.text,
+                                      ),
+                                    );
+                              }
+                            },
+                            buttonLabel: AppStrings.login.toUpperCase(),
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ],
                       ),
                     ),
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
           );
         },
